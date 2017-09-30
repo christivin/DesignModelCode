45e0b3b8687f9d9c7e214082f81944f1415df28d
XuS <544915551@qq.com>
2017-09-30 10:20:15 +0800

    多个工厂方法测试类

--- src/FactoryNormal/MultiFactoryTest.java
+++ src/FactoryNormal/MultiFactoryTest.java
@@ -1,7 +1,18 @@
 package FactoryNormal;
 
 /**
+ * 多个工厂方法测试类
  * Created by Sheng on 2017/9/30.
  */
 public class MultiFactoryTest {
+    public static void main(String[] args) {
+        MultiFactory factory = new MultiFactory();
+        //生产QQ
+        User sender = factory.produceQQ();
+        sender.UserInfo();
+        //生产WeChat
+        sender = factory.produceWeChat();
+        sender.UserInfo();
+    }
 }
+
