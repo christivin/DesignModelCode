c4c8fa2a697ed8f26836a257e18e7e66e7740b0a
XuS <544915551@qq.com>
2017-09-30 10:41:18 +0800

    抽象工厂的实现类-QQ工厂类

---
+++ src/AbstractFactory/QQuserFactory.java
@@ -0,0 +1,20 @@
+package AbstractFactory;
+
+import AbstractFactory.QQUser;
+import AbstractFactory.User;
+import AbstractFactory.WeChatUser;
+
+/**
+ * 抽象工厂的实现类
+ * QQ工厂类
+ * Created by Sheng on 2017/9/30.
+ */
+public class QQuserFactory {
+    public User produceQQ(){
+        return new QQUser();
+    }
+
+    public User produceWeChat(){
+        return new WeChatUser();
+    }
+}
