95493e4bb366a9f4dc6ee085f31002079dd6f5dd
XuS <544915551@qq.com>
2017-10-01 09:21:15 +0800

    单例模式-饿汉模式

--- src/Singleton/HungryModel.java
+++ src/Singleton/HungryModel.java
@@ -4,5 +4,9 @@ package Singleton;
  * Created by Sheng on 2017/10/1.
  */
 public class HungryModel {
-
+    private static HungryModel hungryModel = new HungryModel();
+    private HungryModel (){};
+    public HungryModel getHungryModel(){
+        return hungryModel;
+    }
 }
