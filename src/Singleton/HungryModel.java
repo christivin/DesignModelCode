c57c1786020d71fc47f8e163cad01c521237d4b1
XuS <544915551@qq.com>
2017-10-01 09:23:08 +0800

    单例模式-饿汉模式

--- src/Singleton/HungryModel.java
+++ src/Singleton/HungryModel.java
@@ -1,12 +1,16 @@
 package Singleton;
 
 /**
+ * 这种方式基于classloder机制避免了多线程的同步问题，不过instance在类装载时就实例化，
+ * 虽然导致类装载的原因有很多种，在单例模式中大多数都是调用getInstance方法，
+ * 但是也不能确定有其他的方式（或者其他的静态方法）导致类装载，
+ * 这时候初始化instance显然没有达到lazy loading的效果。
  * Created by Sheng on 2017/10/1.
  */
 public class HungryModel {
-    private static HungryModel hungryModel = new HungryModel();
-    private HungryModel (){};
+    private static HungryModel instance = new HungryModel();
+    private HungryModel (){}
     public HungryModel getHungryModel(){
-        return hungryModel;
+        return instance;
     }
 }
