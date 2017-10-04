236915e4d2f382159875bb1c719225ab464c9058
XuS <544915551@qq.com>
2017-10-04 10:13:24 +0800

    单例模式-枚举方式

--- src/Singleton/DoubleCheckModel.java
+++ src/Singleton/DoubleCheckModel.java
@@ -2,6 +2,10 @@ package Singleton;
 
 /**
  * 双重校验锁
+ * 有人说双重校验没意义
+ 的确在jdk1.5之前是没意义
+ 但在jdk的里程碑版本1.5之后就变得很有意义  而且现在以及被广泛使用  因为引入了修饰符volatile
+ 这也是为什么说jdk1.5才能使用双重校验
  * Created by Sheng on 2017/10/4.
  */
 public class DoubleCheckModel {
