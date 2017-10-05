ca2f7486694351db359fefc8831ddae2943f306d
XuS <544915551@qq.com>
2017-10-05 12:03:20 +0800

    适配器-继承Source实现Targetable

--- src/AdapterModel/Adapter.java
+++ src/AdapterModel/Adapter.java
@@ -7,6 +7,6 @@ public class Adapter extends Source implements Targetable {
 
     @Override
     public void method2(){
-        System.out.println("this is the targetable method!");
+        System.out.println("this is the targetable method!!");
     }
 }
