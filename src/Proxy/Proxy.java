7f963c9edcf8f9986d5e9a086af55e488736b5bc
XuS <544915551@qq.com>
2017-10-07 09:15:09 +0800

    代理模式-代理类

---
+++ src/Proxy/Proxy.java
@@ -0,0 +1,24 @@
+package Proxy;
+
+/**
+ * Created by Sheng on 2017/10/7.
+ */
+class Proxy implements Sourceable{
+    private Source source;
+    public Proxy(){
+        super();
+        this.source=new Source();
+    }
+    @Override
+    public void method(){
+        before();
+        source.method();
+        after();
+    }
+    private void before(){
+        System.out.println("之前");
+    }
+    private void after(){
+        System.out.println("之后");
+    }
+}
