c0f8e9ba328fca9a5b5861b5014bd785e01bf4a5
XuS <544915551@qq.com>
2017-10-10 15:58:04 +0800

    策略模式-实现类-加

---
+++ src/strategy/plus.java
@@ -0,0 +1,13 @@
+package strategy;
+
+/**
+ * Created by Sheng on 2017/10/10.
+ */
+public class plus extends AbstractCalculator implements ICalculator{
+
+    @Override
+    public int calculate(String exp){
+        int[] arrayInt =split(exp,"\\+");
+        return arrayInt[0]+arrayInt[1];
+    }
+}
