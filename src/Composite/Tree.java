41f9d49293121f9c8d7135d74a018cec56834a09
XuS <544915551@qq.com>
2017-10-10 15:16:51 +0800

    组合模式

---
+++ src/Composite/Tree.java
@@ -0,0 +1,25 @@
+package Composite;
+
+/**
+ * 使用场景：将多个对象组合在一起进行操作，常用于表示树形结构中，例如二叉树，数等。
+ * Created by Sheng on 2017/10/9.
+ */
+public class Tree {
+
+    TreeNode root = null;
+
+    public Tree(String name){
+        root = new TreeNode(name);
+    }
+
+    public static void main(String[] args){
+        Tree tree = new Tree("A");
+        TreeNode nodeB = new TreeNode("B");
+        TreeNode nodeC = new TreeNode("C");
+
+        nodeB.add(nodeC);
+        tree.root.add(nodeB);
+        System.out.println("build the tree finished!");
+
+    }
+}
