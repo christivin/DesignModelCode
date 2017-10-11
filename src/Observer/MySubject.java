926e5e50b0ca069f0cbe2b97fcd4149e99740455
XuS <544915551@qq.com>
2017-10-11 09:56:45 +0800

    观察者模式-实际对象

---
+++ src/Observer/MySubject.java
@@ -0,0 +1,13 @@
+package Observer;
+
+/**
+ * 对象
+ * Created by Sheng on 2017/10/11.
+ */
+public class MySubject extends AbstractSubject{
+    @Override
+    public void operation(){
+        System.out.println("update myself");
+        notifyObservers();
+    }
+}
