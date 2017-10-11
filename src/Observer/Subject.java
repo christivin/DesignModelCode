8713dc91b7dde61761e99b971516afe74b530ac5
XuS <544915551@qq.com>
2017-10-11 09:48:43 +0800

    观察者模式-Subject

---
+++ src/Observer/Subject.java
@@ -0,0 +1,20 @@
+package Observer;
+
+/**
+ *
+ * Created by Sheng on 2017/10/11.
+ */
+public interface Subject {
+
+    /*增加观察者*/
+    public void add(Observer observer);
+
+    /*删除观察者*/
+    public void del(Observer observer);
+
+    /*通知所有的观察者*/
+    public void notifyObservers();
+
+    /*自身的操作*/
+    public void operation();
+}
