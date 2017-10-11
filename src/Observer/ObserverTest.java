171ff322434e5f17f75afe2bf0d864dbb678acf0
XuS <544915551@qq.com>
2017-10-11 09:58:15 +0800

    观察者模式-测试类

---
+++ src/Observer/ObserverTest.java
@@ -0,0 +1,14 @@
+package Observer;
+
+/**
+ * Created by Sheng on 2017/10/11.
+ */
+public class ObserverTest {
+    public static void main(String[] args) {
+        Subject sub = new MySubject();
+        sub.add(new Observer1());
+        sub.add(new Observer2());
+
+        sub.operation();
+    }
+}
