e91fd0c845741e8d09885d7b07e793577d053235
XuS <544915551@qq.com>
2017-10-30 11:20:12 +0800

    3

--- src/FactoryNormal/QQUser.java
+++ src/FactoryNormal/QQUser.java
@@ -4,6 +4,7 @@ package FactoryNormal;
  * Created by Sheng on 2017/9/29.
  */
 public class QQUser implements User{
+    @Override
     public void UserInfo(){
         System.out.println("我是QQ用户！");
     }
