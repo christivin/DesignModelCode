eed9e72b2ccec303c349e57a223b69b522718109
XuS <544915551@qq.com>
2017-10-30 11:17:32 +0800

    2

--- src/Facade/Memory.java
+++ src/Facade/Memory.java
@@ -4,11 +4,12 @@ package Facade;
  * Created by Sheng on 2017/10/9.
  */
 public class Memory {
+
     public void startup(){
         System.out.println("Memory Startup!");
     }
 
-    
+
 
     public void shutdown(){
         System.out.println("Memory Shutdown!");
