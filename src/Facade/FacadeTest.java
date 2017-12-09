d895e2063f1c473b2647d1f82b497716dfe08ca2
XuS <544915551@qq.com>
2017-12-09 20:30:44 +0800

    0

--- src/Facade/FacadeTest.java
+++ src/Facade/FacadeTest.java
@@ -10,9 +10,7 @@ public class FacadeTest {
     public static void main(String[] args){
 
         Computer computer = new Computer();
-
         computer.startup();
-
         computer.shutdown();
     }
 }
