4469c8ae8d405ce89f327a49033da4b435659738
XuS <544915551@qq.com>
2017-12-09 20:28:35 +0800

    9

--- src/Facade/Memory.java
+++ src/Facade/Memory.java
@@ -7,7 +7,6 @@ public class Memory {
     public void startup(){
         System.out.println("Memory Startup!");
     }
-
     public void shutdown(){
         System.out.println("Memory Shutdown!");
     }
