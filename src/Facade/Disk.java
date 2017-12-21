33ff52923389ee106806ecc20e32d7199d586a94
XuS <544915551@qq.com>
2017-12-21 15:24:00 +0800

    6

--- src/Facade/Disk.java
+++ src/Facade/Disk.java
@@ -7,6 +7,7 @@ public class Disk {
     public void startup(){
         System.out.println("Disk startup!");
     }
+
     public void shutdown (){
         System.out.println("Disk shutdown!");
     }
