304f5bdd3870b3037bcdbee2b262caa0ea52bca5
XuS <544915551@qq.com>
2017-12-23 15:20:08 +0800

    6

--- src/Prototype/Prototype.java
+++ src/Prototype/Prototype.java
@@ -12,6 +12,7 @@ public class Prototype implements Cloneable {
     public Object clone() throws CloneNotSupportedException {
         Prototype proto = (Prototype) super.clone();
         return proto;
+        
     }
 
 }
